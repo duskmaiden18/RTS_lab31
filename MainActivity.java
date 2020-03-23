29d63c0a2ec98f794c1dc0d945f1f11651382d17
duskmaiden18 <zikzak200015@gmail.com>
2020-03-23 21:45:24 +0200

    add additional task

--- MainActivity.java
+++ MainActivity.java
@@ -4,8 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Завдання: вирішіть проблему зациклення при великому значенні.
+    private static final String VALIDATE_REGEX = "^((?!(0))[0-9]{1,5})$";
+    private AwesomeValidation validation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,34 +22,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View v) {
-        EditText input = findViewById(R.id.number);
         EditText endResult = findViewById(R.id.endResult);
+        EditText input = findViewById(R.id.number);
+        Button button = findViewById(R.id.button);
 
-        final int number = Integer.parseInt(input.getText().toString());
-        final String result = factorize(number);
+        validation = new AwesomeValidation(ValidationStyle.BASIC);
+        validation.addValidation(this, R.id.number, VALIDATE_REGEX 
+                R.string.validate);
 
+        String result = factorize();
         endResult.setText(result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (awesomeValidation.validate()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Everythong correct",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong input",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public String factorize(int n) {
+    public String factorize() {
         String result = "";
+        String text = input.getText().toString();
+
+        if (text.isEmpty()){
+            text = "0";
+        }
+        int n = Integer.parseInt(text);
 
         if (n <= 0) {
             result += n + " ";
             return result;
         }
 
-        if (n % 2 == 0) {
-            //result += " "+  n / 2.0 + ", " + 2;
-            result += "Число має бути непарним";
-            return result;
-        }
-
         int square_root = (int) Math.ceil(Math.sqrt(n));
 
         if (square_root * square_root == n) {
-            System.out.print("[" + square_root + "," + square_root + "]");
-            result += " "+ square_root + ", " + square_root;
+            result += " " + square_root + ", " + square_root;
             return result;
         }
 
